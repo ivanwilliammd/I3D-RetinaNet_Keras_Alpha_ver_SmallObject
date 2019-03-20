26c16df301965f218b91f596af7b62bb7810e0c3
Ivan ASUSDESKTOP <ivan.harsono@binus.ac.id>
2019-03-20 15:05:54 +0700

    revision 3d v1.1.8

--- keras_retinanet/preprocessing/generator.py
+++ keras_retinanet/preprocessing/generator.py
@@ -263,13 +263,15 @@ class Generator(keras.utils.Sequence):
         """ Compute inputs for the network using an image_group.
         """
         # get the max image shape
-        max_shape = tuple(max(image.shape[x] for image in image_group) for x in range(3))
+        # max_shape = tuple(max(image.shape[x] for image in image_group) for x in range(3))
+        max_shape = tuple(max(image.shape[x] for image in image_group) for x in range(4))
 
         # construct an image batch object
         image_batch = np.zeros((self.batch_size,) + max_shape, dtype=keras.backend.floatx())
 
         # copy all images to the upper left part of the image batch object
         for image_index, image in enumerate(image_group):
+            # image_batch[image_index, :image.shape[0], :image.shape[1], :image.shape[2]] = image
             image_batch[image_index, :image.shape[0], :image.shape[1], :image.shape[2], :image.shape[3]] = image
 
         if keras.backend.image_data_format() == 'channels_first':
